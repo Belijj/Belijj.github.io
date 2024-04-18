21699e28a633b98de695ca9c6b90fd7bb074af42
Artemijs Belokrilecs <artjombelich@gmail.com>
2024-04-18 02:07:09 +0200

    2 pages

---
+++ src/home/home.jsx
@@ -0,0 +1,17 @@
+import './home.css';
+import React from 'react';
+
+function Home() {
+    return (
+        <div className='container_home'>
+            <div className='text_part'>
+                <h1>BALTIC MARINE TRAVEL</h1>
+                <p>We work - You fly</p>
+            </div>
+                <div className='photo_part'></div>
+            
+        </div>
+    );
+}
+
+export default Home;

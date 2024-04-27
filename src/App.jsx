43651a6041e43b1619dd34ba88f100f71c23198e
octolss <jeremejev1vladimir@gmail.com>
2024-04-27 21:21:49 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,17 @@ import Home from './home/home.jsx';
 import About from './about/about.jsx';
 import Team from './team/team.jsx';
 import Contacts from './contacts/contacts.jsx';
-<<<<<<< HEAD
 import How from './how_to_works/how.jsx';
 import Links from './links/links.jsx';
-=======
+
 import MobHeader from './mobile/m_header.jsx';
->>>>>>> 0ff66c6c4fcf131e038dd5b856bd24789c7f16cf
+
 
 function App() {
   return (
     <Router>
       <MobHeader />
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
 
@@ -28,12 +26,8 @@ function App() {
 
         <Route path = "/links" element={<Links/>}/>
 
-=======
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/contacts" element={<Contacts />} />
->>>>>>> 0ff66c6c4fcf131e038dd5b856bd24789c7f16cf
+
+        
       </Routes>
     </Router>
   );
--- src/links/links.jsx
+++ src/links/links.jsx
@@ -9,53 +9,24 @@ import nsp from './nsp.jpeg';
 function Links() {
   return (
     <div className="linksReturn">
-        <h1>Links</h1>
-        <p>Our Partners:</p>
-        <ul className='partners'>
-                <li>
-                  <div className="one">
-                      <div className="two">
-                        <a href="https://norbulkshipping.com/">NORBULK SHIPPING UK Ltd</a>
-                      </div>
-                      <div className="two">
-                        <img src={nrb}></img>
-                      </div>
-                  </div>
-                </li>
-                <li><div className="one">
-                      <div className="two">
-                        <a href="https://norbulkshipping.com/">HANZA MARINE MANAGEMENT</a>
-                      </div>
-                      <div className="two">
-                        <img src={hanza}></img>
-                      </div>
-                  </div>
-                </li>
-                <li><div className="one">
-                      <div className="two">
-                        <a href="https://norbulkshipping.com/">SAINT-PETERSBURG MARITIME COMPANY Ltd</a>
-                      </div>
-                      <div className="two">
-                        <img src={snt}></img>
-                      </div>
-                  </div></li>
-                <li><div className="one">
-                      <div className="two">
-                        <a href="https://norbulkshipping.com/">KSM COMPANY Ltd</a>
-                      </div>
-                      <div className="two">
-                        <img src={ksm}></img>
-                      </div>
-                  </div></li>
-                <li><div className="one">
-                      <div className="two">
-                        <a href="https://norbulkshipping.com/">NORBULK PACIFIC MARITIME Inc.</a>
-                      </div>
-                      <div className="two">
-                        <img src={nsp}></img>
-                      </div>
-                  </div></li>
-            </ul>
+        <h1>Our Clients</h1>
+        <div className="partnersContainer">
+              <div className="partner">
+
+              </div>
+              <div className="partner">
+                
+              </div>
+              <div className="partner">
+
+              </div>
+              <div className="partner">
+                
+              </div>
+        </div>
+        <div className="lastPartner">
+
+        </div>  
     </div>
   );
 }

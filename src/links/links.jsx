da67393a418cd717a4974e7be0db747ca44b4282
octolss <jeremejev1vladimir@gmail.com>
2024-04-28 11:52:17 +0300

    .

--- src/links/links.jsx
+++ src/links/links.jsx
@@ -12,23 +12,23 @@ function Links() {
       <div className="partnerContainer">
         <div className="partner">
           <img src={norbulkImage} alt="Norbulk Shipping" />
-          <p><a href=''>NORBULK SHIPPING</a></p>
+          <p><a href='https://norbulkshipping.com/'>NORBULK SHIPPING</a></p>
         </div>
         <div className="partner">
           <img src={hanzaImage} alt="Hanza Marine Management" />
-          <p><a href=''>HANZA MARINE</a></p>
+          <p><a href='https://norbulkshipping.com/'>HANZA MARINE</a></p>
         </div>
         <div className="partner">
           <img src={sntImage} alt="Saint-Petersburg Maritime Company Ltd" />
-          <p><a href=''>SAINT-PETERSBURG</a></p>
+          <p><a href='https://www.spmco.ru/'>SAINT-PETERSBURG</a></p>
         </div>
         <div className="partner">
           <img src={ksmImage} alt="KSM COMPANY Ltd" />
-          <p><a href=''>KSM COMPANY Ltd</a></p>
+          <p><a href='https://www.ksm-world.net/team.html'>KSM COMPANY Ltd</a></p>
         </div>
         <div className="partner">
           <img src={nspImage} alt="NSP Company Ltd" />
-          <p><a href=''>NSP Company Ltd</a></p>
+          <p><a href='https://norbulkshipping.com/'>NSP Company Ltd</a></p>
         </div>
       </div>
     </div>
